b52a05812eeac49f911374b2f3c2314c8d2b1adc
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2005-06-24 14:47:06 +0000

    indepent module revised. And can pass the compiler.
    git-svn-id: http://ucimf.googlecode.com/svn@15 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- iiimccf/iiimccf.h
+++ iiimccf/iiimccf.h
@@ -1,69 +1,44 @@
 #include <iiimcf.h>
+#include <unistd.h>
+#include <stdio.h>
 
 /**
  * @struct IIIMCCF
  * IIIMF input interface.
  */
 typedef struct _IIIMCCF{
+  
+  	// IIIMCF Object
 	IIIMCF_handle 	handle;
 	IIIMCF_context 	context;
 	
-	unsigned char *buf_in, *buf_out;
-	int *buf_in_len, *buf_out_len;
+	// Input and Output buffer
+	const unsigned char *buf_in, *buf_out;
+	int buf_in_len, buf_out_len;
+
+	// Tag
+	int available;
+	int lookup_done;
 	
 	int (*init)();
 	int (*exit)();
 	int (*start)();
 	int (*stop)();
-	int (*handling)();
+	int (*process)();
 } IIIMCCF;
 
+/* API to console */
+IIIMCCF* iiimccf_new( unsigned char* buf_in, int buf_in_len, unsigned char* buf_out, int buf_out_len ); 
+int iiimccf_dispatch();
 
-IIIMCCF* iiimccf_new( unsigned char *buf_in, int *buf_in_len ); 
+/* IIIMCCF member functions*/
+int to_init();
+int to_exit();
+int to_start();
+int to_stop();
+int to_process();
 
-typedef struct _VTInput{	
-	IIIMCF_handle handle;
-	IIIMCF_context context;
-	int available;
-	
-	/* Read_in buffer */
-	unsigned char* buffer;
-	int *length;
-	
-	/* Write_out buffer */
-	unsigned char* mbbuffer;
-	int mb_length;
-	int *fd;
-	
-	/* Tag */
-	int lookup_done;
-	
-} VTInput;
-
-VTInput *VTInput_new( unsigned char* buffer, int *length, int *fd);
-int VTInput_isAvailable( VTInput *vtinput );
-int VTInput_init( VTInput *vtinput );
-int VTInput_exit( VTInput *vtinput );
-int VTInput_start( VTInput *vtinput );
-int VTInput_stop( VTInput *vtinput );
-int VTInput_read_in( VTInput *vtinput );
-int VTInput_handle( VTInput *vtinput );
-int VTInput_reset( VTInput *vtinput );
-int VTInput_write_out( VTInput *vtinput );
-
-int iiimccf_init();
-int iiimccf_exit();
-int iiimccf_start();
-int iiimccf_stop();
-int iiimccf_handle();
-int iiimccf_preedit( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_lookup_choice( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_status( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_commit( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_event_key( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_trigger_notify( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-int iiimccf_aux( IIIMCF_context context,	IIIMCF_event event,	IIIMCF_component current, IIIMCF_component parent );
-
+/* Local utilities function */
 int wchar_to_utf8(wchar_t c, char * outbuf, int bufsize);
 char * iiimcf_text_to_utf8(IIIMCF_text t);
 char * card16_to_utf8(const IIIMP_card16 * pu16);
