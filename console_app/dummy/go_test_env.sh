fbee7cc22e78268bdfac0e5fbaf9d87ed9add0a0
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2006-10-27 04:50:24 +0000

    * test widget, window, graphport...
    git-svn-id: http://ucimf.googlecode.com/svn@114 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- console_app/dummy/go_test_env.sh
+++ console_app/dummy/go_test_env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 LC_CTYPE=zh_TW.UTF-8 \
-LD_LIBRARY_PATH=/tmp/usr/lib/ \
-PATH=/tmp/usr/bin/:$PATH \
+LD_LIBRARY_PATH=/home/mat/usr/lib/ \
+PATH=/home/mat/usr/bin/:$PATH \
+OVMODULE_DIR=/home/mat/usr/lib/openvanilla/ \
+UCIMF_FONTPATH=/usr/X11R6/lib/X11/fonts/misc/b16.pcf.gz \
 /bin/bash;
