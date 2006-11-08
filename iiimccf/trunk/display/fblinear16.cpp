877ee3411b7b0f83962b8d9bcfe14ceaf7b10b4e
mat <mat@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2006-11-08 08:28:05 +0000

    * Add License head, to prepare distribution

--- iiimccf/trunk/display/fblinear16.cpp
+++ iiimccf/trunk/display/fblinear16.cpp
@@ -1,3 +1,22 @@
+// vi:ts=4:shiftwidth=4:expandtab
+/***************************************************************************
+                          fblinear16.cpp  -  description
+                             -------------------
+    begin                : Fri July 20 2001
+    copyright            : (C) 2001 by ejoy, huyong
+    email                : ejoy@users.sourceforge.net
+                           ccpaging@online.sh.cn
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <assert.h>
 #include "fblinear16.h"
 #include <iostream>
