3b539503a1b715cf5b181fe3812d3a14c6c68b72
Jim Huang <jserv.tw@gmail.com>
2007-03-19 19:35:58 +0000

    2007-03-20  Jim Huang  <jserv.tw@gmail.com>
            * .{cpp,h},
            AUTHORS,
            LICENSE: Open RazzmatazZ Laboratory (OrzLab) is now becoming
            the co-owner for UCIMF, which is still licensed under GNU GPL.
            However, externals code from other projects are mentioned.
            NOTE:
            The file of Input Method Interface definition, include/imf/imf.h,
            is licensed under BSD License (3 clause), which implies that
            developing non-GPL Input Method engine for UCIMF is possible once
            UCIMF itself is not modified.
    
    
    git-svn-id: http://ucimf.googlecode.com/svn@159 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- ucimf/trunk/display/fbvgaplanes.cpp
+++ ucimf/trunk/display/fbvgaplanes.cpp
@@ -1,20 +1,28 @@
-// vi:ts=4:shiftwidth=4:expandtab
-/***************************************************************************
-                          fbvgaplanes.cpp  -  description
-                             -------------------
-    begin                : Fri July 28 2001
-    copyright            : (C) 2001 huyong
-    email                : ccpaging@online.sh.cn
- ***************************************************************************/
+/*
+ * UCIMF - Unicode Console InputMethod Framework                                                    
+ *
+ * Copyright (c) 2007 Open RazzmatazZ Laboratory (OrzLab)
+ * Maintained by Chun-Yu Lee (Mat) <Matlinuxer2@gmail.com>
+ *                                                                                                  
+ * Parts taken from zhcon project.
+ * Copyright (c) 2001 ejoy <ejoy@users.sourceforge.net>
+ *                    huyong <ccpaging@online.sh.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
