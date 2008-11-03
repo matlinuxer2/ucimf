b28bbadec012495c3158979595bb06e3e1126f6f
mat0220 <mat0220@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2008-11-03 17:36:57 +0000

    * improve testing script

--- build/debug.sh
+++ build/debug.sh
@@ -4,7 +4,8 @@ source ./inc.sh
 echo "===Testing Console===" | tee  error.log
 LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/fbterm >> error.log
 
-./run.sh
+echo "===Run again message===" | tee -a error.log
+./run.sh 2>&1 error.log >> error.log
 
 if (( $? )) 
 then
@@ -16,6 +17,10 @@ then
 	ls -lR ${BUILD}/lib/ucimf/ | tee -a error.log
 	ls -lR ${BUILD}/lib/openvanilla/ | tee -a error.log
 
+	echo "===Verify Settings===" | tee -a error.log
+	ls -l ${BUILD}/etc/ucimf.conf | tee -a error.log
+	cat   ${BUILD}/etc/ucimf.conf | tee -a error.log
+
 	echo "===Verify Libraries===" | tee -a error.log
 	pkg-config --version freetype2 | tee -a error.log
 	pkg-config --modversion freetype2 | tee -a error.log
@@ -26,5 +31,9 @@ then
 	echo "===Testing console case 01===" | tee -a error.log
 	cd ${BUILD}
 	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console01 >> error.log
+
+	echo "===Testing console case 02===" | tee -a error.log
+	cd ${BUILD}
+	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console02 >> error.log
 fi
 
