1a01b9226705b69d8cc379a3297e4e8cc28f9d42
mat0220 <mat0220@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2008-11-03 14:12:43 +0000

    * add debug test case * improve debug.sh

--- build/debug.sh
+++ build/debug.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 source ./inc.sh
 
-LC_CTYPE=zh_TW.UTF-8 gdb ${BUILD}/bin/fbterm
+echo "===Testing Console===" | tee  error.log
+LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/fbterm >> error.log
+
+./run.sh
+
+if (( $? )) 
+then
+	echo "Console Terminal failed, try testing"
+	build_console_testing
+	cd ${BUILD}
+	echo "===Testing console case 01===" | tee -a error.log
+	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console01 >> error.log
+fi
+
