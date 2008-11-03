a30945d40449a5961ba4e142f1b45fc0e5c1495b
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2008-11-03 14:31:23 +0000

    * improve debug.sh
    git-svn-id: http://ucimf.googlecode.com/svn@208 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- build/debug.sh
+++ build/debug.sh
@@ -8,10 +8,23 @@ LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/fbterm 
 
 if (( $? )) 
 then
-	echo "Console Terminal failed, try testing"
+	echo "===Dump enviroment variables===" | tee -a error.log
+	env >> error.log
+
+	echo "===Verify Files===" | tee -a error.log
+	ls -l ${BUILD}/share/ucimf/ | tee -a error.log
+	ls -lR ${BUILD}/lib/ucimf/ | tee -a error.log
+	ls -lR ${BUILD}/lib/openvanilla/ | tee -a error.log
+
+	echo "===Verify Libraries===" | tee -a error.log
+	pkg-config --version freetype2 | tee -a error.log
+	pkg-config --modversion freetype2 | tee -a error.log
+	
+	echo "===Console Terminal failed, try testing===" | tee -a error.log
 	build_console_testing
-	cd ${BUILD}
+
 	echo "===Testing console case 01===" | tee -a error.log
+	cd ${BUILD}
 	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console01 >> error.log
 fi
 
