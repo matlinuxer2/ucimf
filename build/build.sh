e592e83d50bab696c32f6f47c4f078493189495f
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2008-11-05 20:25:47 +0000

    * add options for jfbterm
    git-svn-id: http://ucimf.googlecode.com/svn@223 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 source ./inc.sh
 
+read -t 10 -p "Choose console: [f(bterm)/j(fbterm)]" myconsole
+
+if [[ $myconsole == "j" ]]
+then
+  echo "Build jfbterm as console program"
+else
+  echo "Build FbTerm as console program"
+fi
+
 build_libucimf
 setup_libucimf
 build_ucimf-openvanilla
 build_openvanilla
-build_console_fbterm
+
+if [[ $myconsole == "j" ]]
+then
+  echo "Build jfbterm as console program"
+  build_console_fbterm
+else
+  echo "Build FbTerm as console program"
+  build_console_jfbterm
+fi
