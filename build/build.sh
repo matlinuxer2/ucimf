fced759704748c270cde9932ca32c8f7f0b14304
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-03-06 18:35:48 +0000

    * build and openvanilla script updated
    git-svn-id: http://ucimf.googlecode.com/svn@276 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- build/build.sh
+++ build/build.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 source ./inc.sh
 
-read -t 10 -p "Choose console: [f(bterm)/j(fbterm)]" myconsole
+#read -t 10 -p "Choose console: [f(bterm)/j(fbterm)]" myconsole
+#
+#if [[ $myconsole == "j" ]]
+#then
+#  echo "Build jfbterm as console program"
+#else
+#  echo "Build FbTerm as console program"
+#fi
 
-if [[ $myconsole == "j" ]]
-then
-  echo "Build jfbterm as console program"
-else
-  echo "Build FbTerm as console program"
-fi
+build_clean
 
 build_libucimf
-setup_libucimf
 build_ucimf-openvanilla
+build_fbterm-ucimf
 build_openvanilla
 
-if [[ $myconsole == "j" ]]
-then
-  echo "Build jfbterm as console program"
-  build_console_jfbterm
-else
-  echo "Build FbTerm as console program"
-  build_console_fbterm
-fi
+
+#if [[ $myconsole == "j" ]]
+#then
+#  echo "Build jfbterm as console program"
+#  build_console_jfbterm
+#else
+#  echo "Build FbTerm as console program"
+#  build_console_fbterm
+#fi
