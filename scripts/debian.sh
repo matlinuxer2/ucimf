e49f4e0a20b64e26cb466b1abbf28b900c46d36e
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-03-08 14:08:58 +0000

    * update debian scripts
    git-svn-id: http://ucimf.googlecode.com/svn@279 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -13,7 +13,8 @@ build_libucimf_deb(){
 	test -L ${LIBUCIMF}/debian && rm ${LIBUCIMF}/debian
 	ln -s  ${SCRIPTS}/debian/libucimf ${LIBUCIMF}/debian
 	cd ${LIBUCIMF}
-	dpkg-buildpackage -rfakeroot && cp -av *.deb ${ROOT} 
+	dpkg-buildpackage -rfakeroot -b 
+	cd .. && rm *.dsc *.changes *.tar.gz && cd -
 	rm ${LIBUCIMF}/debian
 
 	cd ${SCRIPTS}
@@ -23,8 +24,10 @@ build_ucimf-openvanilla_deb(){
 	test -L ${UCIMFOV}/debian && rm ${UCIMFOV}/debian
 	ln -s  ${SCRIPTS}/debian/ucimf-openvanilla ${UCIMFOV}/debian
 	cd ${UCIMFOV}
-	dpkg-buildpackage -rfakeroot && cp -av *.deb ${ROOT} 
+	dpkg-buildpackage -rfakeroot -b 
+	cd .. && rm *.dsc *.changes *.tar.gz && cd -
 	rm ${UCIMFOV}/debian
+	
 
 	cd ${SCRIPTS}
 }
