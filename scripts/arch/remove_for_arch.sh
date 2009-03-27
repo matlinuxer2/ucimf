10f6e2e8c9ce742fdde9566b6173e31e35d01220
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-03-27 19:51:36 +0000

    * add convienient script for Arch Linux
    git-svn-id: http://ucimf.googlecode.com/svn@319 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- scripts/arch/remove_for_arch.sh
+++ scripts/arch/remove_for_arch.sh
@@ -14,3 +14,6 @@ pacman --remove fbterm-svn
 pacman --remove openvanilla-svn 
 pacman --remove ucimf-openvanilla-svn 
 pacman --remove libucimf-svn 
+
+# clean temporary file
+rm -rf /tmp/pacman-tmp-*
