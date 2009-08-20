65ecb322a8f6162ee9b66fc0fb9ec8719b6c8db4
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-08-20 12:32:06 +0000

    * add pre setup for debian
    git-svn-id: http://ucimf.googlecode.com/svn@385 eda2204c-8d1d-0410-b207-95b18c50e4ee

---
+++ scripts/debian/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+aptitude install sudo \
+automake \
+autoconf \
+libtool \
+subversion \
+gnupg-agent\
+debhelper \
+reprepro \
+fakeroot \
+g++ \
+libfreetype6-dev \
+libfontconfig-dev \
+pkg-config \
+lsb-release
