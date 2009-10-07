b9b3a996e5a789f3aafddb52f7c82c81ec5b58be
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-10-07 15:39:57 +0000

    * update build.sh
    git-svn-id: http://ucimf.googlecode.com/svn@390 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- scripts/debian/setup.sh
+++ scripts/debian/setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 aptitude install sudo \
-automake \
-autoconf \
-libtool \
-subversion \
-gnupg-agent\
-debhelper \
-reprepro \
-fakeroot \
-g++ \
-libfreetype6-dev \
-libfontconfig-dev \
-pkg-config \
-lsb-release
+  automake \
+  autoconf \
+  libtool \
+  subversion \
+  gnupg-agent\
+  debhelper \
+  reprepro \
+  fakeroot \
+  g++ \
+  libfreetype6-dev \
+  libfontconfig-dev \
+  libltdl-dev \
+  pkg-config \
+  lsb-release
