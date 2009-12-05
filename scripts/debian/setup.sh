5a7982435f6ebb588dd24db778e4944ce9086235
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2009-12-05 07:56:28 +0000

    * backup ppa_publish script
    git-svn-id: http://ucimf.googlecode.com/svn@402 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- scripts/debian/setup.sh
+++ scripts/debian/setup.sh
@@ -15,3 +15,14 @@ aptitude install sudo \
   libltdl-dev \
   pkg-config \
   lsb-release
+
+
+## # for multiple version
+## SOURCE_DIR="."
+## pushd .
+## cd $SOURCE_DIR
+##         wget http://tjworld.net/raw-attachment/wiki/Linux/Ubuntu/Packages/PPAPublishMultipleReleases/ppa_publish
+##         chmod +x ppa_publish
+##         ./ppa_publish dput-ppa lenny
+## popd
+## 
