4074d06b5b4050178460fe0f89bcaf6de8a09d37
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2010-05-16 09:33:26 +0000

    * Reimplement build release scripts, and deprecates old duplicated works.   This scrips adopt Aron Xu's debianization works for ucimf.   ( Thanks to Aron Xu's great achievement! )
    git-svn-id: http://ucimf.googlecode.com/svn@411 eda2204c-8d1d-0410-b207-95b18c50e4ee

---
+++ scripts/debian/build-packages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+function fetch_launchpad_file(){
+	THE_BRANCH=$1
+	test -d $THE_BRANCH || bzr branch lp:~pkg-ime/ucimf/$THE_BRANCH
+}
+
+function fetch_googlecode_file(){
+	THE_TARBALL=$1
+	test -f $THE_TARBALL || wget --continue http://ucimf.googlecode.com/files/$THE_TARBALL
+}
+
+function find_build_dep(){
+	SEARCH_DIRS=$( echo $@ )
+	if [ $# -gt 0 ]
+        then
+	    echo "There are build dependencies below: "
+	    echo ""
+	    find $SEARCH_DIRS -name 'control' | xargs grep "Build-Depends"| cut -f3- -d: | sed -e "s/(.*)//g" -e "s/,/\n/g" | sort | uniq | xargs echo -n
+	    echo ""
+	    echo ""
+	fi
+
+}
+
+function time_count {
+	COUNT=$1
+
+	for (( x=$COUNT; $x > 0 ; x-- ))
+	do
+		echo -n "$x..."
+		sleep 1
+	done
+}
+
+#### Fetching related source files ####
+####
+#### Thanks to Aron Xu's debianization works!
+####
+fetch_launchpad_file debian-ucimf-openvanilla 
+fetch_launchpad_file debian-openvanilla-modules 
+fetch_launchpad_file debian-libucimf 
+fetch_launchpad_file debian-fbterm-ucimf 
+
+fetch_googlecode_file libucimf-2.2.9.1.tar.gz
+fetch_googlecode_file ucimf-openvanilla-2.10.6.tar.gz
+fetch_googlecode_file openvanilla-modules-0.8.0_14.tar.gz
+fetch_googlecode_file fbterm_ucimf-0.2.6.tar.gz
+
+#### Output possible build dependencies ####
+find_build_dep debian-ucimf-openvanilla debian-openvanilla-modules debian-libucimf debian-fbterm-ucimf 
+time_count 10
+
+#### Make debian packages and merge them into repository tre ####
+TEMP_DIR="incoming"
+
+#### http://code.google.com/p/pkgbzr/
+#### debbing is a debian packaging tool based on debuild and ppa_publish ####
+debbing libucimf-2.2.9.1.tar.gz debian-libucimf/debian $TEMP_DIR 
+debbing ucimf-openvanilla-2.10.6.tar.gz debian-ucimf-openvanilla/debian $TEMP_DIR 
+debbing openvanilla-modules-0.8.0_14.tar.gz debian-openvanilla-modules/debian $TEMP_DIR 
+debbing fbterm_ucimf-0.2.6.tar.gz debian-fbterm-ucimf/debian $TEMP_DIR 
+
+rm -r __debian*
