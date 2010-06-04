96ed271deb15b9f8ae3d44be417fefb2a9bf836a
MatLinuxer2 <MatLinuxer2@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2010-06-04 05:49:42 +0000

    * fixed paths of debus.sh mismatch * Adopt yan's 2nd patch  ( Thanks yan! )

--- scripts/debug/debug.sh
+++ scripts/debug/debug.sh
@@ -1,81 +1,83 @@
 #!/bin/bash
-source ./inc.sh
 
-echo "===Testing Console===" | tee  error.log
-LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/fbterm >> error.log 2>&1
+ROOT="$( dirname $( readlink -f $0))/../.."
+source $ROOT/scripts/env.sh
 
-echo "===Run again message===" | tee -a error.log
-./run.sh 2>>error.log >> error.log
+ERROR_LOG=${BUILD}/error.log
 
+rm ${ERROR_LOG}
+
+echo "===Check SVN Revision===" | tee -a ${ERROR_LOG}
+svn info | grep 'Revision' >> ${ERROR_LOG}
+
+echo "===Dump enviroment variables===" | tee -a ${ERROR_LOG}
+env >> ${ERROR_LOG}
+
+echo "===Check Overlay Files===" | tee -a ${ERROR_LOG}
+echo "Checking ${HOME}/.ucimf.conf" | tee -a ${ERROR_LOG}
+ls -lR ${HOME}/.ucimf.conf | tee -a ${ERROR_LOG}
+
+echo "Checking /etc/ucimf.conf" | tee -a ${ERROR_LOG}
+ls -lR /etc/ucimf.conf | tee -a ${ERROR_LOG}
+
+echo "Checking /lib/ucimf/" | tee -a ${ERROR_LOG}
+ls -lR /lib/ucimf/ | tee -a ${ERROR_LOG}
+
+echo "Checking /lib/openvanilla/" | tee -a ${ERROR_LOG}
+ls -lR /lib/openvanilla/ | tee -a ${ERROR_LOG}
+
+echo "===Verify Files===" | tee -a ${ERROR_LOG}
+ls -lR ${BUILD}/share/ucimf/ | tee -a ${ERROR_LOG}
+ls -lR ${BUILD}/lib/ucimf/ | tee -a ${ERROR_LOG}
+ls -lR ${BUILD}/lib/openvanilla/ | tee -a ${ERROR_LOG}
+
+echo "===Verify Settings===" | tee -a ${ERROR_LOG}
+ls -lR ${BUILD}/etc/ | tee -a ${ERROR_LOG}
+cat   ${BUILD}/etc/ucimf.conf | tee -a ${ERROR_LOG}
+
+echo "===Verify Libraries===" | tee -a ${ERROR_LOG}
+pkg-config --version freetype2 | tee -a ${ERROR_LOG}
+pkg-config --modversion freetype2 | tee -a ${ERROR_LOG}
+
+
+echo "===Testing Console===" | tee -a ${ERROR_LOG}
+LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${SCRIPTS}/debug/trace.gdb fbterm >> ${ERROR_LOG} 2>&1
+
+echo "===Run again message===" | tee -a ${ERROR_LOG}
+./run.sh 2>>${ERROR_LOG} >> ${ERROR_LOG}
+
+	
+echo "===Console Terminal failed, try testing===" | tee -a ${ERROR_LOG}
+build_console_testing
+
+echo "===Testing console case 01===" | tee -a ${ERROR_LOG}
+cd ${BUILD}
+LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${SCRIPTS}/debug/trace.gdb ${BUILD}/bin/console01 >>${ERROR_LOG} 2>&1
+
+echo "===Run again message===" | tee -a ${ERROR_LOG}
+LC_CTYPE=zh_TW.UTF-8 ${BUILD}/bin/console01 >>${ERROR_LOG} 2>&1
 if (( $? )) 
 then
-	echo "===Check SVN Revision===" | tee -a error.log
-	svn info | grep 'Revision' >> error.log
-
-	echo "===Dump enviroment variables===" | tee -a error.log
-	env >> error.log
-
-	echo "===Check Overlay Files===" | tee -a error.log
-	echo "Checking ${HOME}/.ucimf.conf" | tee -a error.log
-	ls -lR ${HOME}/.ucimf.conf | tee -a error.log
-
-	echo "Checking /etc/ucimf.conf" | tee -a error.log
-	ls -lR /etc/ucimf.conf | tee -a error.log
-
-	echo "Checking /lib/ucimf/" | tee -a error.log
-	ls -lR /lib/ucimf/ | tee -a error.log
-
-	echo "Checking /lib/openvanilla/" | tee -a error.log
-	ls -lR /lib/openvanilla/ | tee -a error.log
-
-	echo "===Verify Files===" | tee -a error.log
-	ls -lR ${BUILD}/share/ucimf/ | tee -a error.log
-	ls -lR ${BUILD}/lib/ucimf/ | tee -a error.log
-	ls -lR ${BUILD}/lib/openvanilla/ | tee -a error.log
-
-	echo "===Verify Settings===" | tee -a error.log
-	ls -lR ${BUILD}/etc/ | tee -a error.log
-	cat   ${BUILD}/etc/ucimf.conf | tee -a error.log
-
-	echo "===Verify Libraries===" | tee -a error.log
-	pkg-config --version freetype2 | tee -a error.log
-	pkg-config --modversion freetype2 | tee -a error.log
-	
-	echo "===Console Terminal failed, try testing===" | tee -a error.log
-	build_console_testing
-
-	echo "===Testing console case 01===" | tee -a error.log
-	cd ${BUILD}
-	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console01 >>error.log 2>&1
-
-	echo "===Run again message===" | tee -a error.log
-	LC_CTYPE=zh_TW.UTF-8 ${BUILD}/bin/console01 >>error.log 2>&1
-	if (( $? )) 
-	then
-		echo "====Rebuild libucimf==="
-		build_libucimf
-		setup_libucimf
-	fi
-
-	echo "===Testing console case 02===" | tee -a error.log
-	cd ${BUILD}
-	LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console02 >>error.log 2>&1
-
-	echo "===Testing console case 03===" | tee -a error.log
-	cd ${BUILD}
-	FONT_PATH=${BUILD}/share/ucimf/unifont.pcf gdb -batch -x ${BUILD}/debug/trace.gdb ${BUILD}/bin/console03 >>error.log 2>&1
+	echo "====Rebuild libucimf==="
+	build_libucimf
+	setup_libucimf
 fi
+
+echo "===Testing console case 02===" | tee -a ${ERROR_LOG}
+cd ${BUILD}
+LC_CTYPE=zh_TW.UTF-8 gdb -batch -x ${SCRIPTS}/debug/trace.gdb ${BUILD}/bin/console02 >>${ERROR_LOG} 2>&1
+
+echo "===Testing console case 03===" | tee -a ${ERROR_LOG}
+cd ${BUILD}
+FONT_PATH=${BUILD}/share/ucimf/unifont.pcf gdb -batch -x ${SCRIPTS}/debug/trace.gdb ${BUILD}/bin/console03 >>${ERROR_LOG} 2>&1
 
 
 # Create convient bug report pack
 test -d ${BUILD}/BugReport && rm -rf ${BUILD}/BugReport 
 
 mkdir -p ${BUILD}/BugReport
-cp ${BUILD}/../libucimf/config.log  ${BUILD}/BugReport/libucimf.config.log
-cp ${BUILD}/../ucimf-openvanilla/config.log  ${BUILD}/BugReport/ucimf_openvanilla.config.log
-cp ${BUILD}/../console/jfbterm/jfbterm-0.4.7/config.log ${BUILD}/BugReport/jfbterm.config.log
-cp ${BUILD}/../console/fbterm/fbterm-1.2/config.log ${BUILD}/BugReport/fbterm.config.log
-cp ${BUILD}/../console/dummy/config.log ${BUILD}/BugReport/dummy.config.log
-cp ${BUILD}/../OpenVanilla/Modules/config.log ${BUILD}/BugReport/OpenVanillaModules.config.log
+cp ${ROOT}/libucimf/config.log  ${BUILD}/BugReport/libucimf.config.log
+cp ${ROOT}/ucimf-openvanilla/config.log  ${BUILD}/BugReport/ucimf_openvanilla.config.log
+cp ${ROOT}/console/fbterm_ucimf/config.log ${BUILD}/BugReport/fbterm_ucimf.config.log
 cd ${BUILD}
 tar -czf BugReport.tar.gz BugReport/
