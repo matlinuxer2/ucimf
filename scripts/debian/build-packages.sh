23d2c41e0d43915e18bcf8761f358376939fc980
MatLinuxer2 <MatLinuxer2@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2010-06-09 07:55:33 +0000

    * remove deprecated part * update some scripts * adjust ucimf_start menu behavior

--- scripts/debian/build-packages.sh
+++ scripts/debian/build-packages.sh
@@ -43,23 +43,23 @@ fetch_launchpad_file debian-openvanilla-modules
 fetch_launchpad_file debian-libucimf 
 fetch_launchpad_file debian-fbterm-ucimf 
 
-fetch_googlecode_file libucimf-2.2.9.1.tar.gz
-fetch_googlecode_file ucimf-openvanilla-2.10.6.tar.gz
+fetch_googlecode_file libucimf-2.3.1.tar.gz
+fetch_googlecode_file ucimf-openvanilla-2.10.7.tar.gz
 fetch_googlecode_file openvanilla-modules-0.8.0_14.tar.gz
-fetch_googlecode_file fbterm_ucimf-0.2.6.tar.gz
+fetch_googlecode_file fbterm_ucimf-0.2.7.tar.gz
 
 #### Output possible build dependencies ####
 find_build_dep debian-ucimf-openvanilla debian-openvanilla-modules debian-libucimf debian-fbterm-ucimf 
-time_count 10
+time_count 5
 
 #### Make debian packages and merge them into repository tre ####
 TEMP_DIR="incoming"
 
 #### http://code.google.com/p/pkgbzr/
 #### debbing is a debian packaging tool based on debuild and ppa_publish ####
-debbing libucimf-2.2.9.1.tar.gz debian-libucimf/debian $TEMP_DIR 
-debbing ucimf-openvanilla-2.10.6.tar.gz debian-ucimf-openvanilla/debian $TEMP_DIR 
+debbing libucimf-2.3.1.tar.gz debian-libucimf/debian $TEMP_DIR 
+debbing ucimf-openvanilla-2.10.7.tar.gz debian-ucimf-openvanilla/debian $TEMP_DIR 
 debbing openvanilla-modules-0.8.0_14.tar.gz debian-openvanilla-modules/debian $TEMP_DIR 
-debbing fbterm_ucimf-0.2.6.tar.gz debian-fbterm-ucimf/debian $TEMP_DIR 
+debbing fbterm_ucimf-0.2.7.tar.gz debian-fbterm-ucimf/debian $TEMP_DIR 
 
 rm -r __debian*
