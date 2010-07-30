9904938c9da5fb1512b18814c37ca79187059650
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2010-07-30 17:49:05 +0000

    Update ArchLinux build scripts.
    git-svn-id: http://ucimf.googlecode.com/svn@517 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- trunk/scripts/arch/install_for_arch_from_svn.sh
+++ trunk/scripts/arch/install_for_arch_from_svn.sh
@@ -1,75 +1,33 @@
 #!/bin/bash
 
-SCRIPTS=$(pwd)
-PKGBUILD=${SCRIPTS}/pkgbuilds/
-test -d ${PKGBUILD} || mkdir -p ${PKGBUILD}
+ROOT="$( readlink -f $(dirname $0)/../..)" 
+SCRIPTS_ARCH="$ROOT/scripts/arch/"
 
-back_to_scripts(){
-	cd ${SCRIPTS}
+
+fetch_file_from () {
+	local NAME="$1"
+	local URL="$2"
+
+	if [ ! -f $NAME ]; then
+		wget -O "$NAME" "$URL"
+	fi
 }
 
-make_pkgbuild_of_libucimf-svn(){
-	echo "Start to make pkgbuild of libucimf..."
-	cd ${PKGBUILD}
-	test -f PKGBUILD_libucimf || wget -O PKGBUILD_libucimf http://aur.archlinux.org/packages/libucimf-svn/libucimf-svn/PKGBUILD
-	makepkg -ip PKGBUILD_libucimf
+make_pkgbuild_from_aur () {
+	local PKGNAME="$1"
 
-	back_to_scripts
+	install -d "$SCRIPTS_ARCH/$PKGNAME"
+
+	pushd . ; cd "$SCRIPTS_ARCH/$PKGNAME"
+		fetch_file_from PKGBUILD http://aur.archlinux.org/packages/$PKGNAME/$PKGNAME/PKGBUILD
+		makepkg -i
+	popd
 }
 
-make_pkgbuild_of_ucimf-openvanilla-svn(){
-	echo "Start to make pkgbuild of ucimf-openvanilla..."
-	cd ${PKGBUILD}
-	test -f PKGBUILD_ucimf-openvanilla || wget -O PKGBUILD_ucimf-openvanilla http://aur.archlinux.org/packages/ucimf-openvanilla-svn/ucimf-openvanilla-svn/PKGBUILD
-	makepkg -ip PKGBUILD_ucimf-openvanilla
+make_pkgbuild_from_aur libucimf-svn
+make_pkgbuild_from_aur ucimf-openvanilla-svn
+make_pkgbuild_from_aur openvanilla-modules-svn
+make_pkgbuild_from_aur fbterm-ucimf-svn
+make_pkgbuild_from_aur fbterm
 
-	back_to_scripts
-}
-
-make_pkgbuild_of_openvanilla-svn(){
-	echo "Start to make pkgbuild of openvanilla..."
-	cd ${PKGBUILD}
-	test -f PKGBUILD_openvanilla || wget -O PKGBUILD_openvanilla http://aur.archlinux.org/packages/openvanilla-svn/openvanilla-svn/PKGBUILD
-	makepkg -ip PKGBUILD_openvanilla
-
-	back_to_scripts
-}
-
-make_pkgbuild_of_fbterm-ucimf-svn(){
-	echo "Start to make pkgbuild of fbterm-ucimf..."
-	cd ${PKGBUILD}
-	test -f PKGBUILD_fbterm-ucimf || wget -O PKGBUILD_fbterm-ucimf http://aur.archlinux.org/packages/ucimf-fbterm-svn/ucimf-fbterm-svn/PKGBUILD
-	makepkg -ip PKGBUILD_fbterm-ucimf
-
-	back_to_scripts
-}
-
-make_pkgbuild_of_fbterm(){
-	echo "Start to make pkgbuild of fbterm"
-	cd ${PKGBUILD}
-	yaourt --noconfirm --sync fbterm
-	#test -f PKGBUILD_fbterm || wget -O PKGBUILD_fbterm http://aur.archlinux.org/packages/fbterm/fbterm/PKGBUILD
-	#makepkg -ip PKGBUILD_fbterm
-
-	back_to_scripts
-}
-
-
-# commands
-#make_pkgbuild_of_libucimf-svn
-#make_pkgbuild_of_ucimf-openvanilla-svn
-#make_pkgbuild_of_openvanilla-svn
-#make_pkgbuild_of_fbterm-ucimf-svn
-#make_pkgbuild_of_fbterm
-
-yaourt -S --noconfirm libucimf-svn
-yaourt -S --noconfirm ucimf-openvanilla-svn
-yaourt -S --noconfirm openvanilla-svn
-yaourt -S --noconfirm fbterm-svn
-yaourt -S --noconfirm fbterm-ucimf-svn
-
-pacman -Q |grep ^libucimf
-pacman -Q |grep ^ucimf-openvanilla
-pacman -Q |grep ^openvanilla
-pacman -Q |grep ^fbterm-ucimf
-pacman -Q |grep ^fbterm |grep -v ucimf
+pacman -Q |egrep "ucimf|openvanilla|fbterm"
