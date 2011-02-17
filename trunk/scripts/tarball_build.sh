8e46ac566cacd0ca3e552e148d9d7435060c0c98
MatLinuxer2 <MatLinuxer2@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2011-02-17 12:24:07 +0000

    Release ucimf-chewing 0.3 Fixed duplicated preedit text and pass review.

--- trunk/scripts/tarball_build.sh
+++ trunk/scripts/tarball_build.sh
@@ -47,7 +47,7 @@ function prepare_tarball(){
 prepare_tarball libucimf 2.3.8
 prepare_tarball ucimf-openvanilla 2.10.11
 prepare_tarball ucimf-sunpinyin 0.4
-prepare_tarball ucimf-chewing 0.2
+prepare_tarball ucimf-chewing 0.3
 prepare_tarball openvanilla-modules 0.9.0a1.3
 prepare_tarball fbterm_ucimf 0.2.9
 
