75a75adc23fdb29b685e44709419b3206f958c52
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2011-07-30 02:44:09 +0000

    * update scripts for gentoo
    git-svn-id: http://ucimf.googlecode.com/svn@547 eda2204c-8d1d-0410-b207-95b18c50e4ee

--- trunk/scripts/gentoo/unmerge.sh
+++ trunk/scripts/gentoo/unmerge.sh
@@ -4,4 +4,6 @@ emerge --unmerge fbterm-ucimf
 emerge --unmerge libucimf
 emerge --unmerge ucimf-openvanilla
 emerge --unmerge openvanilla-modules 
+emerge --unmerge ucimf-sunpinyin
+emerge --unmerge ucimf-chewing
 qlist -Iv openvanilla ucimf fbterm 
