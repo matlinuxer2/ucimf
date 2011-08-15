fe6d90c23b79df1dc7ad980750073580427242e5
MatLinuxer2 <MatLinuxer2@eda2204c-8d1d-0410-b207-95b18c50e4ee>
2011-08-15 15:12:10 +0000

    * update gentoo related repository data.

--- trunk/scripts/gentoo/merge_svn.sh
+++ trunk/scripts/gentoo/merge_svn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ACCEPT_KEYWORDS="**" 可以 enable *-9999.ebuild 的 svn 套件
-layman -s gentoo-taiwan
+layman -s gentoo-zh
 USE="ucimf" ACCEPT_KEYWORDS="~*" emerge jfbterm 
 ACCEPT_KEYWORDS="**" emerge fbterm fbterm-ucimf libucimf ucimf-openvanilla openvanilla-modules ucimf-sunpinyin ucimf-chewing
 qlist -Iv openvanilla ucimf fbterm 
