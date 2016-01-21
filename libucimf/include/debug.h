0541778fc99b19ba7f0ca359e3d6576d752b2365
ChangZhuo Chen (陳昌倬) <(none)>
2016-01-21 21:19:03 +0800

    Fix GCC 6 error

--- libucimf/include/debug.h
+++ libucimf/include/debug.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define UCIMF_ERR(format...)    	{extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[Err]:"format  );} }
-#define UCIMF_WARNING(format...)        {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[WARN]:"format );} } 
-#define UCIMF_INFO(format...)           {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[INFO]:"format );} } 
-#define UCIMF_DEBUG(format...)          {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[DEBUG]:"format);} } 
+#define UCIMF_ERR(format, ...)    	{extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[Err]:"format, ##__VA_ARGS__);} }
+#define UCIMF_WARNING(format, ...)        {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[WARN]:"format, ##__VA_ARGS__);} }
+#define UCIMF_INFO(format, ...)           {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[INFO]:"format, ##__VA_ARGS__);} }
+#define UCIMF_DEBUG(format, ...)          {extern int LogFd; if( LogFd >=0 ){ dprintf( LogFd, "[DEBUG]:"format, ##__VA_ARGS__);} }
 
-#define UrINFO(format...) UCIMF_INFO(format)
-#define UrDEBUG(format...) UCIMF_DEBUG(format)
+#define UrINFO(format, ...) UCIMF_INFO(format, ##__VA_ARGS__)
+#define UrDEBUG(format, ...) UCIMF_DEBUG(format, ##__VA_ARGS__)
